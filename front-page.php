0781c704e8dbfc3d41c4004a2e9c480fa0024164
Adriana Veloso Meireles <dricaveloso@gmail.com>
2015-10-14 09:42:44 -0300

    texto

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@ get_header(); ?>
 <section class="container-fluid velho-sniic"><div class="wrapper center-block clearfix row">
 <aside class="clearfix">
 	<img class="center-block col-xs-offset-1 col-sm-1 col-sm-offset-0" src="<?php echo get_template_directory_uri() ?>/assets/old-logo.svg"/>
-	<p class="center-block col-sm-5 text-center">Conheça e explore a nova plataforma do Sistema Nacional de Indicadores e Informações Culturais .</p>
+	<p class="center-block col-sm-5 text-center">Conheça e explore a nova plataforma do Sistema Nacional de Informações e Indicadores Culturais .</p>
 	<button class="btn btn-default col-xs-12 col-sm-3 col-sm-offset-1" type="button">Cadastro SNIIC</button>
 </aside>
 </div></section>
@@ -26,7 +26,7 @@ get_header(); ?>
 	<p class="col-xs-12 col-sm-6 col-md-4">Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
 	<figure class="col-xs-12 col-sm-12 col-md-4">
 		<img src="http://lorempixel.com/400/200/sports/Dummy-Text" class="img-responsive">
-		<figcaption>Navegue pelo Mapa da Cultura.</figcaption>
+		<figcaption>Navegue pelo Mapa da Cultura</figcaption>
 	</figure>
 </div></section>
 
@@ -36,7 +36,7 @@ get_header(); ?>
 	<p class="col-sm-offset-1">Indicadores.cultura: Painéis de visualização de indicadores, na forma de gráficos e tabelas, de políticas ou informações culturais. O primeiro painel lançado será com os dados do programa Vale Cultura, que trará informações sempre atualizadas sobre a distribuição e utilização deste benefício. Em seguida publicaremos um painel com dados da Lei Rouanet e outro com as informações das pesquisas de Perfis de Municípios (MUNIC) realizada pelo IBGE e que terá sua nova edição lançada no final deste ano.</p>
 	<p class="col-sm-offset-1">Publicações.cultura: Repositório de pesquisas, artigos, estudos, produtos de consultorias e muitas outras publicações de interesse sobre indicadores e informações culturais relevantes para as políticas culturais.</p>
 	<p class="col-sm-offset-1">Vocabulario.cultura: Espaço para publicação das taxonomias e ontologias da cultura. A proposta, em andamento através de um Grupo de Trabalho (GT) e de uma parceria com a Universidade Federal de Goiânia (UFG), é consolidar este vocabulário, para ser utilizado por todos os módulos do SNIIC e também para servir de recomendação para pesquisadores e gestores que queiram desenvolver outros sistemas culturais. Estes vocabulários serão publicados em formato de fácil compreensão, assim como em formatos da web semântica.</p>
-	<p class="col-sm-offset-1"> Dados.cultura: Portal de dados abertos, nos mesmos moldes do dados.gov.br </p>
+	<p class="col-sm-offset-1">Dados.cultura: Portal de dados abertos, nos mesmos moldes do dados.gov.br </p>
 
 </div>
 <aside class="col-xs-12 col-sm-5 col-sm-offset-1 plataformas">
