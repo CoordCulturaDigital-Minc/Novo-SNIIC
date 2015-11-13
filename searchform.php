47235ecafb1b278010cd45506d6a2685b195c86c
Tiago Mergulhão <me@tmergulhao.com>
2015-11-13 13:57:22 -0200

    Simpler compact header format

--- searchform.php
+++ searchform.php
@@ -1,11 +1,12 @@
-<form role="search" method="get" class="search-form" action="<?php bloginfo('siteurl'); ?>">
-	<label>
+<form role="search" method="get" class="search-form clearfix" action="<?php bloginfo('siteurl'); ?>"><label>
 		<span class="sr-only">Buscar:</span>
-		<input type="search" class="search-field col-xs-12" placeholder="O que você está buscando?" value="<?php
+
+		<input type="search" class="search-field" placeholder="O que você está buscando?" value="<?php
 			echo get_search_query();
 		?>" name="s" title="Search for:">
-	</label>
-	<button type="submit" class="search-submit" value="Search">
-			<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-	</button>
-</form>
+
+		<button type="submit" class="search-submit" value="Search">
+				<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+		</button>
+
+</label></form>
