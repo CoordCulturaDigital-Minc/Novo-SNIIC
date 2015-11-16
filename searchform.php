17ab8790bb216dd766939210b548c3678a11c3a4
Tiago Mergulhão <me@tmergulhao.com>
2015-11-16 17:47:02 -0200

    Cleanse and Bootstrap compliance

--- searchform.php
+++ searchform.php
@@ -1,13 +1,19 @@
 <form role="search" method="get" class="search-form clearfix" action="<?php bloginfo('siteurl'); ?>">
-	<label class="has-glyph-button">
-		<span class="sr-only">Buscar:</span>
+	<div class="form-group">
+		<!-- <label class="hidden" for="searchForm">Buscar</label> -->
+		<span class="sr-only">Buscar</span>
 
-		<input type="search" class="search-field" placeholder="O que você está buscando?" value="<?php
-			echo get_search_query();
-		?>" name="s" title="Search for:">
+		<div class="input-group">
+			<input type="search" class="search-field" placeholder="O que você está buscando?" value="<?php
+				echo get_search_query();
+			?>" name="s" title="Search for:">
+			<!-- id="searchForm" -->
+			<div class="input-group-addon">
+				<button type="submit" class="search-submit" value="Search">
+					<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+				</button>
+			</div>
+		</div>
 
-		<button type="submit" class="search-submit" value="Search">
-				<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-		</button>
-
-</label></form>
+	</div>
+</form>
