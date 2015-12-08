b56bff7340bba6b1d1f2cad8d907d888c7fee0eb
Tiago Mergulhão <me@tmergulhao.com>
2015-12-08 11:02:09 -0200

    Label call fix

--- shortcodes.php
+++ shortcodes.php
@@ -10,7 +10,7 @@ function spagobi_dashboard_shortcode( $atts ) {
 		), $atts )
 	);
 
-	return '<iframe frameborder="no" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBISDK/verdocspago.jsp?documentLabel=' . $documentlabel . '" height="'.$height.'" ></iframe>';
+	return '<iframe frameborder="no" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBISDK/verdocspago.jsp?documentLabel=' . $label . '" height="'.$height.'" ></iframe>';
 }
 add_shortcode( 'spagobi_dash', 'spagobi_dashboard_shortcode' );
 
@@ -23,7 +23,7 @@ function spagobi_chart_shortcode( $atts ) {
 		), $atts )
 	);
 
-	return '<iframe frameborder="no" height="'.$height.'" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBIChartEngine/servlet/AdapterHTTP?&user_id=sefic&SPAGOBI_AUDIT_ID=3527&EXECUTION_CONTEXT=DOCUMENT_COMPOSITION&DOCUMENT_LABEL=' . $documentlabel . '&DOCUMENT_COMMUNITIES=[]&DOCUMENT_IS_VISIBLE=true&SBICONTEXT=/SpagoBI&ACTION_NAME=CHART_ENGINE_START_ACTION&DOCUMENT_FUNCTIONALITIES=[14, 3]&DOCUMENT_AUTHOR=biadmin&DOCUMENT_DESCRIPTION=&document=26&IS_TECHNICAL_USER=false&language=en&country=US&SBI_EXECUTION_ID=7caf6699a6df4c6db71f65c475aed4df&DOCUMENT_NAME=_&NEW_SESSION=TRUE&DOCUMENT_IS_PUBLIC=true&DOCUMENT_VERSION=271&SBI_HOST=http://spagobi.lab.cultura.gov.br&MODALITY=VIEW&SBI_LANGUAGE=pt&SBI_COUNTRY=BR&ROLE=/spagobi/sefic"></iframe>';
+	return '<iframe frameborder="no" height="'.$height.'" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBIChartEngine/servlet/AdapterHTTP?&user_id=sefic&SPAGOBI_AUDIT_ID=3527&EXECUTION_CONTEXT=DOCUMENT_COMPOSITION&DOCUMENT_LABEL=' . $label . '&DOCUMENT_COMMUNITIES=[]&DOCUMENT_IS_VISIBLE=true&SBICONTEXT=/SpagoBI&ACTION_NAME=CHART_ENGINE_START_ACTION&DOCUMENT_FUNCTIONALITIES=[14, 3]&DOCUMENT_AUTHOR=biadmin&DOCUMENT_DESCRIPTION=&document=26&IS_TECHNICAL_USER=false&language=en&country=US&SBI_EXECUTION_ID=7caf6699a6df4c6db71f65c475aed4df&DOCUMENT_NAME=_&NEW_SESSION=TRUE&DOCUMENT_IS_PUBLIC=true&DOCUMENT_VERSION=271&SBI_HOST=http://spagobi.lab.cultura.gov.br&MODALITY=VIEW&SBI_LANGUAGE=pt&SBI_COUNTRY=BR&ROLE=/spagobi/sefic"></iframe>';
 }
 add_shortcode( 'spagobi_chart', 'spagobi_chart_shortcode' );
 
