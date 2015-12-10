5ee2f39be3220f683029ea00431664b29c33bf74
Tiago Mergulhão <me@tmergulhao.com>
2015-12-10 15:11:57 -0200

    Template betterments and new shades and tones for charts

--- shortcodes.php
+++ shortcodes.php
@@ -10,22 +10,9 @@ function spagobi_dashboard_shortcode( $atts ) {
 		), $atts )
 	);
 
-	return '<iframe frameborder="no" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBISDK/verdocspago.jsp?documentLabel=' . $label . '" height="'.$height.'" ></iframe>';
+	return do_shortcode('[twbs_frameless]' '<iframe frameborder="no" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBISDK/verdocspago.jsp?documentLabel=' . $label . '" height="'.$height.'" ></iframe>' . '[/twbs_frameless]');
 }
-add_shortcode( 'spagobi_dash', 'spagobi_dashboard_shortcode' );
-
-function spagobi_chart_shortcode( $atts ) {
-	extract( shortcode_atts(
-		array(
-			'label' => '', //nomes de atributo sao todos lowercase
-			'height' => '500px',
-			'width' => '100%',
-		), $atts )
-	);
-
-	return '<iframe frameborder="no" height="'.$height.'" width="' . $width . '" scrolling="no" src="http://spagobi.lab.cultura.gov.br/SpagoBIChartEngine/servlet/AdapterHTTP?&user_id=sefic&SPAGOBI_AUDIT_ID=3527&EXECUTION_CONTEXT=DOCUMENT_COMPOSITION&DOCUMENT_LABEL=' . $label . '&DOCUMENT_COMMUNITIES=[]&DOCUMENT_IS_VISIBLE=true&SBICONTEXT=/SpagoBI&ACTION_NAME=CHART_ENGINE_START_ACTION&DOCUMENT_FUNCTIONALITIES=[14, 3]&DOCUMENT_AUTHOR=biadmin&DOCUMENT_DESCRIPTION=&document=26&IS_TECHNICAL_USER=false&language=en&country=US&SBI_EXECUTION_ID=7caf6699a6df4c6db71f65c475aed4df&DOCUMENT_NAME=_&NEW_SESSION=TRUE&DOCUMENT_IS_PUBLIC=true&DOCUMENT_VERSION=271&SBI_HOST=http://spagobi.lab.cultura.gov.br&MODALITY=VIEW&SBI_LANGUAGE=pt&SBI_COUNTRY=BR&ROLE=/spagobi/sefic"></iframe>';
-}
-add_shortcode( 'spagobi_chart', 'spagobi_chart_shortcode' );
+add_shortcode( 'spagobi', 'spagobi_dashboard_shortcode' );
 
 function bootstrap_text_wrapper($atts, $content = null) {
 	extract( shortcode_atts(
