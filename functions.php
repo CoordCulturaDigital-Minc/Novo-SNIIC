be2ceb4b27497d03cf66de16cba4c0063e3664e7
Tiago Mergulhão <me@tmergulhao.com>
2015-12-28 14:14:01 -0200

    Google Fonts instead of Adobe Typekit

--- functions.php
+++ functions.php
@@ -252,6 +252,9 @@ function argent_neue_dependencies () {
 	// FONT-AWESOME
 	wp_enqueue_style( 'font-awesome', $bower_directory . '/font-awesome/css/font-awesome.min.css' );
 
+	// LATO@GOOGLE-FONTS
+	wp_enqueue_style( 'lato-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' );
+
 	// TWITTER BOOTSTRAP
 	wp_enqueue_style( 'twitter_bootstrap', $bower_directory . '/bootstrap/dist/css/bootstrap.min.css' );
 	wp_enqueue_style( 'twitter_bootstrap_theme', $bower_directory . '/bootstrap/dist/css/bootstrap-theme.min.css' );
